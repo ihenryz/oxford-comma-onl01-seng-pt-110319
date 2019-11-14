40b708c6daa1e8c26f2a904854e4d9cc77c2bb0a
Henry Phan <h.phan06@gmail.com>
2019-11-14 23:37:45 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
-def oxford_comma(array)
+require 'pry'
 
+
+def oxford_comma(array)
+  if array.length < 2 
+    array.join
+    elsif array.length == 2
+    array.join(" and ")
+    elsif array.length == 3
+    array[-1].prepend("and ")
+    array.join(", ")
+  else array.length > 3
+    array[-1].prepend("and ")
+    array.join(", ")
+  end
 end
